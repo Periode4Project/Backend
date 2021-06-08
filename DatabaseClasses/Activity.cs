4cb52efa6b86ee8bc4bc98249fdd41842c084c41
dennisrijsdijk <dennis.rijsdijk@student.nhlstenden.com>
2021-06-08 19:00:06 +0200

    Database Class for Activity

---
+++ DatabaseClasses/Activity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SailingBackend.DatabaseClasses
+{
+    public class Activity
+    {
+        public int ActivityID { get; set; }
+        public string ActivityImage { get; set; }
+        public string ActivityName { get; set; }
+        public string ActivityDesc { get; set; }
+        public string ActivityType { get; set; }
+        public double EntranceFee { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public double Lat { get; set; }
+        public double Lng { get; set; }
+    }
+}
