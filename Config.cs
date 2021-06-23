28e1faf0bd98a3fdb4c89907cba65b3874666372
dennisrijsdijk <dennis.rijsdijk@student.nhlstenden.com>
2021-06-23 22:20:14 +0200

    generate config by constructor

--- Config.cs
+++ Config.cs
@@ -10,23 +10,22 @@ namespace SailingBackend
 {
     public static class Config
     {
-        public static readonly ConfigFile config = GetDatabaseConfig();
+        public static ConfigFile Database { get; set; }
 
-        public static ConfigFile GetDatabaseConfig()
+        static Config() 
         {
-            ConfigFile databaseConfig = new ConfigFile()
-            {
-                Username = "root",
-                Password = "root",
-                Database = "sailing",
-                Host = "127.0.0.1",
-                Port = 3306,
-                MapQuestToken = "https://developer.mapquest.com/user/me/apps"
-            };
+             Database = new ConfigFile()
+             {
+                 Username = "root",
+                 Password = "root",
+                 Database = "sailing",
+                 Host = "127.0.0.1",
+                 Port = 3306
+             };
 
             if (!File.Exists(@"config.json"))
             {
-                string json = JsonConvert.SerializeObject(databaseConfig, Formatting.Indented);
+                string json = JsonConvert.SerializeObject(Database, Formatting.Indented);
                 using (StreamWriter outFile = new StreamWriter(@"config.json"))
                 {
                     outFile.Write(json);
@@ -35,9 +34,8 @@ namespace SailingBackend
             using (StreamReader file = File.OpenText(@"config.json"))
             {
                 JsonSerializer serializer = new JsonSerializer();
-                databaseConfig = (ConfigFile)serializer.Deserialize(file, typeof(ConfigFile));
+                Database = (ConfigFile)serializer.Deserialize(file, typeof(ConfigFile));
             }
-            return databaseConfig;
         }
     }
 }
