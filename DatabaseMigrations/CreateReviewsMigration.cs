2ba2e7b192cb18e961a1043127a6b08c59adf82f
dennisrijsdijk <dennis.rijsdijk@student.nhlstenden.com>
2021-06-24 18:03:29 +0200

    CreateReviewsMigration

--- DatabaseMigrations/CreateReviewsMigration.cs
+++ DatabaseMigrations/CreateReviewsMigration.cs
@@ -5,8 +5,14 @@ using System.Threading.Tasks;
 
 namespace SailingBackend.DatabaseMigrations
 {
+    /// <summary>
+    /// Migration Class
+    /// </summary>
     public static class CreateReviewsMigration
     {
+        /// <summary>
+        /// Static Migration
+        /// </summary>
         public static DatabaseMigration DatabaseMigration { get; set; } = new DatabaseMigration 
         { 
             Queries = new List<Query> { } 
@@ -16,17 +22,20 @@ namespace SailingBackend.DatabaseMigrations
             DatabaseMigration.MigrationName = "AddReviews";
             DatabaseMigration.Queries.Add(new Query
             {
-                SqlQuery = @"CREATE TABLE Reviews(
-                ReviewID INTEGER NOT NULL AUTO_INCREMENT,
-                ReviewTitle VARCHAR(100) NOT NULL,
-                Rating INTEGER NOT NULL,
-                ReviewDESC VARCHAR(1000) NOT NULL,
-                ReviewWriter INTEGER NOT NULL,
-                Activity INTEGER NOT NULL,
-                PRIMARY KEY(ReviewID),
-                FOREIGN KEY(ReviewWriter) REFERENCES Users(UserID),
-                FOREIGN KEY(Activity) REFERENCES Activities(ActivityID)
-                )",
+                SqlQuery = @"
+                CREATE TABLE Reviews
+                (
+                    ReviewID INTEGER NOT NULL AUTO_INCREMENT,
+                    ReviewTitle VARCHAR(100) NOT NULL,
+                    Rating INTEGER NOT NULL,
+                    ReviewDESC VARCHAR(1000) NOT NULL,
+                    ReviewWriter INTEGER NOT NULL,
+                    Activity INTEGER NOT NULL,
+                    PRIMARY KEY(ReviewID),
+                    FOREIGN KEY(ReviewWriter) REFERENCES Users(UserID),
+                    FOREIGN KEY(Activity) REFERENCES Activities(ActivityID)
+                )
+                ",
                 Params = new { }
             });
             DatabaseMigration.Queries.Add(new Query
