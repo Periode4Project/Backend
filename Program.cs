8bb815c74164562344a66ba0515e089c9f5c702d
dennisrijsdijk <dennis.rijsdijk@student.nhlstenden.com>
2021-06-24 17:45:33 +0200

    Default ASP methods

--- Program.cs
+++ Program.cs
@@ -10,8 +10,15 @@ using SailingBackend.DatabaseMigrations;
 
 namespace SailingBackend
 {
+    /// <summary>
+    /// Default program class
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Default entrypoint
+        /// </summary>
+        /// <param name="args"> console arguments </param>
         public static void Main(string[] args)
         {
             if (args.Contains("--migrate"))
@@ -19,6 +26,11 @@ namespace SailingBackend
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Default Hostbuilder
+        /// </summary>
+        /// <param name="args"> entrypoint args </param>
+        /// <returns> IHostBuilder </returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
