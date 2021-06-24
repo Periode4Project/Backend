64a5f4f5a2b2304f82652ad5cf0617bb897eb930
dennisrijsdijk <dennis.rijsdijk@student.nhlstenden.com>
2021-06-24 17:19:17 +0200

    ApplicationClass Activity

--- ApplicationClasses/Activity.cs
+++ ApplicationClasses/Activity.cs
@@ -5,26 +5,71 @@ using System.Threading.Tasks;
 
 namespace SailingBackend.ApplicationClasses
 {
+    /// <summary>
+    /// Activity Application-side
+    /// </summary>
     public class Activity
     {
+        /// <summary>
+        /// Activity ID
+        /// </summary>
         public int ActivityId { get; set; }
+        /// <summary>
+        /// Activity Image URL
+        /// </summary>
         public string ActivityImage { get; set; }
+        /// <summary>
+        /// Activity Name
+        /// </summary>
         public string ActivityName { get; set; }
+        /// <summary>
+        /// Actvity Description
+        /// </summary>
         public string ActivityDesc { get; set; }
+        /// <summary>
+        /// Activity Type
+        /// </summary>
         public string ActivityType { get; set; }
+        /// <summary>
+        /// Activity Place
+        /// </summary>
         public Place ActivityPlace { get; set; }
+        /// <summary>
+        /// Entrance Fee
+        /// </summary>
         public double EntranceFee { get; set; }
     }
+    /// <summary>
+    /// Place Type
+    /// </summary>
     public class Place
     {
+        /// <summary>
+        /// City
+        /// </summary>
         public string City { get; set; }
+        /// <summary>
+        /// Address
+        /// </summary>
         public string Address { get; set; }
+        /// <summary>
+        /// Location
+        /// </summary>
         public Location Location { get; set; }
     }
 
+    /// <summary>
+    /// Location Type
+    /// </summary>
     public class Location
     {
+        /// <summary>
+        /// Latitude
+        /// </summary>
         public double lat { get; set; }
+        /// <summary>
+        /// Longtitude
+        /// </summary>
         public double lng { get; set; }
     }
 }
