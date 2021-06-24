46936b707bc78dc08b8c71302d7120f743e23c02
dennisrijsdijk <dennis.rijsdijk@student.nhlstenden.com>
2021-06-24 17:30:03 +0200

    Review class

--- ApplicationClasses/Review.cs
+++ ApplicationClasses/Review.cs
@@ -5,14 +5,38 @@ using System.Threading.Tasks;
 
 namespace SailingBackend.ApplicationClasses
 {
+    /// <summary>
+    /// Review
+    /// </summary>
     public class Review
     {
+        /// <summary>
+        /// ReviewID
+        /// </summary>
         public int ReviewID { get; set; }
+        /// <summary>
+        /// ReviewTitle
+        /// </summary>
         public string ReviewTitle { get; set; }
+        /// <summary>
+        /// Rating
+        /// </summary>
         public double Rating { get; set; }
+        /// <summary>
+        /// Review Description
+        /// </summary>
         public string ReviewDESC { get; set; }
+        /// <summary>
+        /// Review Writer ID
+        /// </summary>
         public int ReviewWriter { get; set; }
+        /// <summary>
+        /// Review Writer Name
+        /// </summary>
         public string ReviewWriterName { get; set; }
+        /// <summary>
+        /// Activity ID
+        /// </summary>
         public int Activity { get; set; }
     }
 }
