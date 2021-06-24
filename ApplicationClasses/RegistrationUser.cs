60f825f9e69dd2de9eacc17d1c20a61f186c43b0
dennisrijsdijk <dennis.rijsdijk@student.nhlstenden.com>
2021-06-24 17:22:50 +0200

    RegistrationUser

--- ApplicationClasses/RegistrationUser.cs
+++ ApplicationClasses/RegistrationUser.cs
@@ -5,10 +5,22 @@ using System.Threading.Tasks;
 
 namespace SailingBackend.ApplicationClasses
 {
+    /// <summary>
+    /// user registration Info
+    /// </summary>
     public class RegistrationUser
     {
+        /// <summary>
+        /// Email
+        /// </summary>
         public string Email { get; set; }
+        /// <summary>
+        /// Full Name
+        /// </summary>
         public string FullName { get; set; }
+        /// <summary>
+        /// Password
+        /// </summary>
         public string Password { get; set; }
     }
 }
