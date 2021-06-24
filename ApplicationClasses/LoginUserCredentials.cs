a6ca2d55780cae5c83494dbe8276591528097897
dennisrijsdijk <dennis.rijsdijk@student.nhlstenden.com>
2021-06-24 17:22:01 +0200

    LoginuserCredentials

--- ApplicationClasses/LoginUserCredentials.cs
+++ ApplicationClasses/LoginUserCredentials.cs
@@ -5,9 +5,18 @@ using System.Threading.Tasks;
 
 namespace SailingBackend.ApplicationClasses
 {
+    /// <summary>
+    /// Class to hold email and password for auth purposes
+    /// </summary>
     public class LoginUserCredentials
     {
+        /// <summary>
+        /// Email
+        /// </summary>
         public string Email { get; set; }
+        /// <summary>
+        /// Password
+        /// </summary>
         public string Password { get; set; }
     }
 }
